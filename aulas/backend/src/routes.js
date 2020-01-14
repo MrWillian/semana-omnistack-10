fe7eb7b832183ba4abdd9891a9aecd2537a3d383
MrWilllian <willian.dev10@gmail.com>
2020-01-14 15:03:15 -0300

    feat: add rules of business

--- aulas/backend/src/routes.js
+++ aulas/backend/src/routes.js
@@ -1,16 +1,33 @@
 const { Router } = require('express');
 const axios = require('axios');
+const Dev = require('./models/Dev');
 
 const routes = Router();
 
 routes.post('/devs', async (request, response) => {
-  const { github_username } = request.body;
+  const { github_username, techs, latitude, longitude } = request.body;
 
   const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
 
-  console.log(apiResponse.data);
+  const { name = login, avatar_url, bio } = apiResponse.data;
 
-  return response.json({ message: 'Hello OmniStack'});
+  const techsArray = techs.split(',').map(tech => tech.trim());
+
+  const location = {
+    type: 'Point',
+    coordinates: [longitude, latitude],
+  }
+
+  const dev = await Dev.create({
+    github_username, 
+    name,
+    avatar_url, 
+    bio, 
+    techs: techsArray,
+    location
+  });
+
+  return response.json(dev);
 });
 
 module.exports = routes;
