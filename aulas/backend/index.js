fca831d5ccdbbcdfcbdcfda133a5cc64ef100f1b
MrWilllian <willian.dev10@gmail.com>
2020-01-14 14:27:57 -0300

    feat: add mongoose

--- aulas/backend/index.js
+++ aulas/backend/index.js
@@ -1,6 +1,14 @@
-const express = require ('express');
+const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-58x2s.mongodb.net/week10?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+app.use(express.json());
 
 app.get('');
 
