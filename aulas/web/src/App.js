19b139d02feab4919c87f66292c5b84580eb83d1
MrWilllian <willian.dev10@gmail.com>
2020-01-15 15:11:02 -0300

    feat: add DevForm component

--- aulas/web/src/App.js
+++ aulas/web/src/App.js
@@ -6,31 +6,11 @@ import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
+import DevForm from './components/DevForm';
 import DevItem from './components/DevItem';
 
 function App() {
   const [devs, setDevs] = useState([]);
-
-  const [github_username, setGithubUsername] = useState('');
-  const [techs, setTechs] = useState('');
-  const [latitude, setLatitude] = useState('');
-  const [longitude, setLongitude] = useState('');
-
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords;
-        setLatitude(latitude);
-        setLongitude(longitude);
-      },
-      (err) => { 
-        console.log(err); 
-      },
-      {
-        timeout: 30000, 
-      }
-    );
-  }, []);
 
   useEffect(() => {
     async function loadDevs() {
@@ -40,19 +20,8 @@ function App() {
     loadDevs();
   }, []);
 
-  async function handleAddDev(e) {
-    e.preventDefault();
-
-    const response = await api.post('/devs', {
-      github_username,
-      techs,
-      latitude,
-      longitude
-    });
-
-    setGithubUsername('');
-    setTechs('');
-
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data);
     setDevs([...devs, response.data]);
   }
 
@@ -60,37 +29,7 @@ function App() {
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form onSubmit={handleAddDev}>
-          <div className="input-block">            
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input type="text" name="github_username" id="github_username" 
-            value={github_username} onChange={e => setGithubUsername(e.target.value)}
-            required />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input type="text" name="techs" id="techs" 
-            value={techs} onChange={e => setTechs(e.target.value)}
-            required />
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input type="number" name="latitude" id="latitude" 
-              value={latitude} onChange={e => setLatitude(e.target.value)} required />
-            </div>
-            
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input type="number" name="longitude" id="longitude" 
-              value={longitude} onChange={e => setLongitude(e.target.value)} required />
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
 
       <main>
