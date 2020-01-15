c0e528d7a3dfe408e05fc063e9748842e83560e1
MrWilllian <willian.dev10@gmail.com>
2020-01-15 14:59:55 -0300

    feat: add insert and list of devs in frontend web

--- aulas/web/src/App.js
+++ aulas/web/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import './global.css';
 import './App.css';
@@ -6,6 +7,8 @@ import './Sidebar.css';
 import './Main.css';
 
 function App() {
+  const [devs, setDevs] = useState([]);
+
   const [github_username, setGithubUsername] = useState('');
   const [techs, setTechs] = useState('');
   const [latitude, setLatitude] = useState('');
@@ -18,19 +21,37 @@ function App() {
         setLatitude(latitude);
         setLongitude(longitude);
       },
-      (err) => {
-        console.log(err);
+      (err) => { 
+        console.log(err); 
       },
       {
-        timeout: 30000,
+        timeout: 30000, 
       }
     );
+  }, []);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+      setDevs(response.data);
+    }
+    loadDevs();
   }, []);
 
   async function handleAddDev(e) {
     e.preventDefault();
 
-    
+    const response = await api.post('/devs', {
+      github_username,
+      techs,
+      latitude,
+      longitude
+    });
+
+    setGithubUsername('');
+    setTechs('');
+
+    setDevs([...devs, response.data]);
   }
 
   return (
@@ -72,50 +93,19 @@ function App() {
 
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/50757994?v=4" alt="MrWillian" />
-              <div className="user-info">
-                <strong>Willian Marciel</strong>
-                <span>NodeJS, ReactJS, React Native</span>
-              </div>
-            </header>
-            <p>Mobile Developer</p>
-            <a href="https://github.com/MrWillian">Acessar Perfil no GitHub</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/50757994?v=4" alt="MrWillian" />
-              <div className="user-info">
-                <strong>Willian Marciel</strong>
-                <span>NodeJS, ReactJS, React Native</span>
-              </div>
-            </header>
-            <p>Mobile Developer</p>
-            <a href="https://github.com/MrWillian">Acessar Perfil no GitHub</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/50757994?v=4" alt="MrWillian" />
-              <div className="user-info">
-                <strong>Willian Marciel</strong>
-                <span>NodeJS, ReactJS, React Native</span>
-              </div>
-            </header>
-            <p>Mobile Developer</p>
-            <a href="https://github.com/MrWillian">Acessar Perfil no GitHub</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/50757994?v=4" alt="MrWillian" />
-              <div className="user-info">
-                <strong>Willian Marciel</strong>
-                <span>NodeJS, ReactJS, React Native</span>
-              </div>
-            </header>
-            <p>Mobile Developer</p>
-            <a href="https://github.com/MrWillian">Acessar Perfil no GitHub</a>
-          </li>
+          {devs.map(dev => (
+            <li key={dev._id} className="dev-item">
+              <header>
+                <img src={dev.avatar_url} alt={dev.name} />
+                <div className="user-info">
+                  <strong>{dev.name}</strong>
+                  <span>{dev.techs.join(', ')}</span>
+                </div>
+              </header>
+              <p>{dev.bio}</p>
+              <a href={`https://github.com/${dev.github_username}`}>Acessar Perfil no GitHub</a>
+            </li>
+          ))}
         </ul>
       </main>
     </div>
