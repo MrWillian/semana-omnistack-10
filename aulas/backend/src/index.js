69854c3261cf08735a6fe468fb0975ad1b6eb362
MrWilllian <willian.dev10@gmail.com>
2020-01-17 08:17:35 -0300

    feat: add socket io configs

--- aulas/backend/src/index.js
+++ aulas/backend/src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const routes = require('./routes');
 const cors = require('cors');
+const http = require('http');
+const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-58x2s.mongodb.net/week10?retryWrites=true&w=majority', {
   useNewUrlParser: true,
